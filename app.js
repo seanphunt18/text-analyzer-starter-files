56c36dccc63ca4787a1863efa7fdbd256283c8a4
seanphunt18 <seanphunt18@gmail.com>
2017-02-28 10:01:55 -0500

    correct js

--- app.js
+++ app.js
@@ -1,48 +1,38 @@
 // your code here!
 
 function countWords(entry) {
-	var wordCount = 0;
-	for (i=0; i<entry.length; i++) {
-		wordCount = i;
-	}
-	return wordCount;
-	$('.js-word-count').append(wordCount);
+	$('.js-word-count').text(entry.length);
 }
 
 function countUniqueWords(entry) {
 	var uniqueWords = 0;
-	var wordLibrary = [];
+	var wordLibrary = {};
 	for (i=0; i<entry.length; i++) {
 		if (!(entry[i] in wordLibrary)) {
 			uniqueWords++;
-			wordLibrary.push(entry[i]);
+			wordLibrary[entry[i]] = 1;
 		}
 	}
-	return uniqueWords;
-	$('.js-unique-word-count').append(uniqueWords);	
+	$('.js-unique-word-count').text(uniqueWords);	
 }
 
 function calculateAverageLength(entry) {
-	var wordLengths = [];
-	var avgLength = 0;
+	var wordCount = entry.length;
 	var total = 0;
-	for (i=0; i<entry.length; i++) {
-		wordLengths.push(entry[i].length);
+	for (var i=0; i<entry.length; i++) {
+		total += entry[i].length;
 	}
-	for (var i in wordLengths) {
-		total += wordLengths[i];
-	}
-	avgLength = (total / wordLengths.length);
-	return avgLength;
-	$('.js-avg-word-length').append(avgLength);
+	avgLength = (total / wordCount);
+	$('.js-avg-word-length').text(avgLength.toFixed(2));
 }
 
 $(function() {
 	$('button[type=submit]').click(function(e) {
-		$('.hidden').removeClass('hidden');
-		countWords('#user-text');
-		countUniqueWords('#user-text');
-		calculateAverageLength('#user-text');
 		e.preventDefault();
-	})
-})
+		$('.hidden').removeClass('hidden');
+		var sentenceText = $('#user-text').val().split(' ');
+		countWords(sentenceText);
+		countUniqueWords(sentenceText);
+		calculateAverageLength(sentenceText);
+	});
+});
